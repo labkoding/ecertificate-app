6ad25ed6eecde00acff84b403b8d2769cfe5be28
Nofrets Poai <opetstudio@gmail.com>
2022-08-25 01:57:23 +0800

    integrasi dengan react-navigation, untuk setiap halaman kita kumpulkan ke folder screens

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,17 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import HomeScreen from './screens/HomeScreen'
+
+const Stack = createStackNavigator()
 
 function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName='HomeScreen' headerMode='none'>
+        <Stack.Screen name='HomeScreen' component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
 export default App
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
